82750ea4c877e22b3678bafc97e97fa9883a5e91
wudong <915001258@qq.com>
2017-12-05 23:46:59 +0800

    实战，小项目

--- flask-shizhan/shizhan.py
+++ flask-shizhan/shizhan.py
@@ -1,5 +1,5 @@
 #encoding: utf-8
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import config
 
 app = Flask(__name__)
@@ -8,6 +8,13 @@ app.config.from_object(config)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login/',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
 
 
 if __name__ == '__main__':
